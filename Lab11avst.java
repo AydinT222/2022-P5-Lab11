abee308eed5835cf4b411be9e2dbe2c51e8fccfb
Aydin <70951773+AydinT222@users.noreply.github.com>
2023-01-06 10:59:13 -0500

    lab 100 point ver

--- Lab11avst.java
+++ Lab11avst.java
@@ -3,6 +3,7 @@
 // This is the student, starting version of the Lab11a assignment.
 
 
+import java.text.DecimalFormat;
 import java.util.Scanner;
 
 
@@ -12,24 +13,50 @@ public class Lab11avst
     {
         // This main method needs additions for the 100 point version.
         Scanner input = new Scanner(System.in);
-        final int MAX = 100;
-        boolean primes[];
-        primes = new boolean[MAX];
-        computePrimes(primes);
-        displayPrimes(primes);
+        System.out.print("Enter the primes upper bound ===>> ");
+        try {
+            final int MAX = input.nextInt();
+            boolean primes[] = new boolean[MAX];
+
+            computePrimes(primes);
+            displayPrimes(primes);
+        } catch (Exception e) {
+            if (e.hashCode() == 1836019240) { // input mismatch
+                System.out.println("Please ensure that you entered in an integer, and not a character or a very large integer");
+            } else {
+                throw e;
+            }
+        }
     }
 
     public static void computePrimes(boolean primes[])
     {
         System.out.println("\nCOMPUTING PRIME NUMBERS");
-
+        for (int i = 2; i < primes.length; i++) {
+            primes[i] = true;
+        }
+        for(int i = 2; i < primes.length; i++) {
+            for (int k = i * 2; k < primes.length; k += i) {
+                primes[k] = false;
+            }
+        }
     }
 
     public static void displayPrimes(boolean primes[])
     {
         System.out.println("\n\nPRIMES BETWEEN 1 AND "+ primes.length);
         System.out.println();
-
+        int counter = 0;
+        for (int i = 0; i < primes.length; i++) {
+            if (primes[i]) {
+                if (counter >= 15) {
+                    counter = 0;
+                    System.out.println();
+                }
+                counter++;
+                System.out.print(i + " ");
+            }
+        }
     }
 
 }
